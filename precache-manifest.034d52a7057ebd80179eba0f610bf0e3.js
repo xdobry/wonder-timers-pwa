5f30b03b60527e728765e816c3c5b1bb4bad3212
Trzewik <A.Trzewik@t-systems.com>
2020-05-25 12:42:28 +0200

    pwa

---
+++ precache-manifest.034d52a7057ebd80179eba0f610bf0e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7914e79cd985eb6151b",
+    "url": "/wonder-timers-pwa/css/chunk-vendors.de7b1f33.css"
+  },
+  {
+    "revision": "1647bbc02609b43980d77b717fdb2d8e",
+    "url": "/wonder-timers-pwa/index.html"
+  },
+  {
+    "revision": "e36f9973ee5f4c36ce14",
+    "url": "/wonder-timers-pwa/js/app.b5df8469.js"
+  },
+  {
+    "revision": "b7914e79cd985eb6151b",
+    "url": "/wonder-timers-pwa/js/chunk-vendors.20489eb1.js"
+  },
+  {
+    "revision": "5ec016a25617de5efcdf7ad1ffbf6482",
+    "url": "/wonder-timers-pwa/manifest.json"
+  },
+  {
+    "revision": "3715f0bbf23b6b8c8d8820b53a633c9c",
+    "url": "/wonder-timers-pwa/media/alarm.3715f0bb.ogg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/wonder-timers-pwa/robots.txt"
+  }
+]);
