12f6307b1dc92124d203746ca80a7dea9547bcf4
Artur T <mail@xdobry.de>
2026-03-30 23:35:46 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/wonder-timers-pwa/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
